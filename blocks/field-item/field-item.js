35f2190826809451f56a8698e8e0f4ea2dad3386
Michael Ippolito <mikeippi123>
2025-12-02 16:31:08 -0500

    updating component

--- blocks/field-item/field-item.js
+++ blocks/field-item/field-item.js
@@ -1,4 +1,4 @@
-export default function decorate(block) {
+export default function decorate() {
   // The field-item block acts as a container wrapper
   // It applies styling through CSS classes without modifying the DOM structure
   // This allows the Universal Editor to properly manage child components
